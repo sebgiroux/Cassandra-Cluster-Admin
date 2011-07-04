565b0510f35cb63273d7ff1daa7540e7839c4300
Sebastien Giroux <sebastien@bolidea.com>
2011-07-04 19:50:55 -0400

    Add getting started section to README

--- include/conf.inc.php
+++ include/conf.inc.php
@@ -6,7 +6,7 @@
 		@copyright All rights reserved - 2011
 	*/
 
-	$CASSANDRA_SERVERS = array('127.0.0.1:9160'); // Add as many Cassandra nodes as you want in this array
+	$CASSANDRA_SERVERS = array('127.0.0.1:9160'); // Add as many Cassandra nodes as you want in this array like so: array('10.70.80.90:9160','10.70.80.100:9160','10.70.80.110:9160')
 	
 	/*
 		Enter your Cassandra credentials here if needed

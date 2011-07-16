1101ec73d28c0f7cb10438d351e2b4664719b2d1
Sebastien Giroux <sebastien@bolidea.com>
2011-07-16 15:57:24 -0400

    Fix missing </div>

--- views/columnfamily_row.php
+++ views/columnfamily_row.php
@@ -3,8 +3,13 @@
 		<tr>
 			<td colspan="2">
 				<div class="float_left"><?=$scf_key?></div>
-				<div class="float_right" style="margin-left: 20px;"><div class="delete_row_icon"></div><div class="float_left"><a href="#" onclick="deleteSuperColumn('<?php echo $keyspace_name; ?>','<?php echo $columnfamily_name; ?>','<?php echo $row_key; ?>','<?php echo $scf_key;?>')">Delete</a></div>
-				<div class="clear_both"></div>
+				<div class="float_right" style="margin-left: 20px;">
+					<div class="delete_row_icon"></div>
+					<div class="float_left">
+						<a href="#" onclick="deleteSuperColumn('<?php echo $keyspace_name; ?>','<?php echo $columnfamily_name; ?>','<?php echo $row_key; ?>','<?php echo $scf_key;?>')">Delete</a>
+					</div>
+					<div class="clear_both"></div>
+				</div>
 			</td>
 		</tr>
 	<?php endif; ?>

70910a60c5b9745f7dd166576c795f1c44ea76a2
Sebastien Giroux <sebastien@bolidea.com>
2012-05-28 22:45:28 -0400

    Missing escape in JS

--- views/columnfamily_browse_data_row.php
+++ views/columnfamily_browse_data_row.php
@@ -4,7 +4,7 @@
 	<?php if ($show_actions_link): ?>
 	<td>
 		<?php if (!$is_counter_column):?><div class="edit_row_icon"></div><div class="float_left"><a href="columnfamily_action.php?action=edit_row&amp;keyspace_name=<?php echo $keyspace_name; ?>&amp;columnfamily_name=<?php echo $columnfamily_name; ?>&key=<?php echo $key; ?>">Edit</a> | </div><?php endif; ?>
-		<div class="small_horizontal_spacer"></div><div class="delete_row_icon"></div><div class="float_left"><a href="#" onclick="deleteRow('<?php echo $keyspace_name; ?>','<?php echo $columnfamily_name; ?>','<?php echo $key; ?>')">Delete</a></div>
+		<div class="small_horizontal_spacer"></div><div class="delete_row_icon"></div><div class="float_left"><a href="#" onclick="deleteRow('<?php echo $keyspace_name; ?>','<?php echo $columnfamily_name; ?>','<?php echo escapeNameForJs($key); ?>')">Delete</a></div>
 	</td>
 	<?php endif; ?>
 </tr>
